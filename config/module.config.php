d79d4dd418de946165a3e39cec6c51226f19a383
northmule <izm@zixn.ru>
2021-11-21 20:08:17 +0300

    fix code

--- config/module.config.php
+++ config/module.config.php
@@ -1,19 +1,16 @@
 <?php
 
-use Interop\Container\ContainerInterface;
-
 return [
     'service_manager' => [
         'factories'  => [
             Northmule\Telegram\Options\ModuleOptions::class          => Northmule\Telegram\Options\Factory\ModuleOptions::class,
-            Northmule\Telegram\Commands\NewchatmembersCommand::class => Northmule\Telegram\Commands\Factory\NewchatMembers::class,
             Northmule\Telegram\Events\Events::class                  => Northmule\Telegram\Events\Factory\Events::class,
             Northmule\Telegram\Service\TelegramApi::class            => Northmule\Telegram\Service\Factory\TelegramApi::class,
         ],
         'invokables' => [
-            Northmule\Telegram\Service\KeyboardQuestion::class => Northmule\Telegram\Service\KeyboardQuestion::class,
-            Northmule\Telegram\Service\TelegramRestrict::class => Northmule\Telegram\Service\TelegramRestrict::class,
-            Laminas\EventManager\EventManager::class           => Laminas\EventManager\EventManager::class,
+            Northmule\Telegram\Service\KeyboardQuestion::class,
+            Northmule\Telegram\Service\TelegramRestrict::class,
+            Laminas\EventManager\EventManager::class,
 
         ],
         'aliases'    => [],
